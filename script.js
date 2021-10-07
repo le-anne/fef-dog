e8080d36f5b2d73e63cf5db45ca6037bbefde35a
Leanne <76232496+le-anne@users.noreply.github.com>
2021-10-07 13:55:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-if (location.protocol == "http:") {
-  location.protocol = "https:";
+if (location.protocol == 'http:') {
+  location.protocol = 'https:';
 }
 
 const app = new Vue({
-  el: "#app",
-  data: {
-    currentDogUrl: null,
-    favourites: [],
-    online: true,
-  },
-  methods: {
-    loadDog: async function() {
-      const response = await fetch("https://dog.ceo/api/breeds/image/random");
-      const asJson = await response.json();
-      this.currentDogUrl = asJson.message;
-      console.log(this.currentDogUrl);
+    el: '#app',
+    data: {
+      currentDogUrl: null,
+      favourites: [],
+      online: true,
     },
-    addFavourite: async function() {
-      if (!this.favourites.includes(this.currentDogUrl)) {
-              this.favourites.push(this.currentDogUrl);
-        
-        const imageCache = await caches.open("favouriteImageCache")
-        imageCache.add(this.currentDogUrl);
-      }
-      this.loadDog();
-    },
-    removeFav: function(dog) {
-      this.favourites = this.favourites.filter(item => item !== dog);
-    }
-  },
-  created() {
-    this.loadDog();
-},
-  mounted() {
-    if (localStorage.favourites) {
-      this.favourites = JSON.parse(localStorage.favourites);
-    }
-    
-    const onlineStatus = navigator.onLine;
-    addEventListener("offline", () => {
-      this.onLine = false;
-    })
-    addEventListener("online", () => {
-      this.onLine = true;
-    })
-  },
-  watch: {
-    favourites(updatedFavourites) {
-      localStorage.favourites = JSON.stringify(updatedFavourites)
-    },
-    online(isOnline) {
-      if(isOnline) {
+    methods: {
+      loadDog: async function () {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const asJson = await response.json();
+        this.currentDogUrl = asJson.message;
+        console.log(this.currentDogUrl);
+      },
+      addFav: async function() {
+        if (!this.favourites.includes(this.currentDogUrl)) {
+          this.favourites.push(this.currentDogUrl);
+          
+          const imageCache = await caches.open("favouriteImageCache");
+          imageCache.add(this.currentDogUrl);
+        }
         this.loadDog();
+      },
+      removeFav: function(dog) {
+        this.favourites = this.favourites.filter(item => item !== dog);
+      }
+    },
+    created() {
+       this.loadDog();
+    },
+    mounted() {
+      if (localStorage.favourites) {
+        this.favourites = JSON.parse(localStorage.favourites);
+      }
+      
+      const onlineStatus = navigator.onLine;
+      addEventListener("offline", () => {
+        this.online = false;
+      });
+      addEventListener("online", () => {
+        this.online = true;
+      });
+    },
+    watch: {
+      favourites(updatedFavourites) {
+        localStorage.favourites = JSON.stringify(updatedFavourites)
+      },
+      online(isOnline) {
+        if(isOnline) {
+          this.loadDog();
+        }
       }
     }
-  }
 });
 
 if ("serviceWorker" in navigator) {
