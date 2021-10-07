1a4f3dd25ff57ecbb7d26ec95f2b491c05f71ddd
Leanne <76232496+le-anne@users.noreply.github.com>
2021-10-07 13:28:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,14 @@ const app = new Vue({
   }
 });
 
-navigator.serviceWorkerContainer
-      .register("sw.js")
-      .then(function(reg){
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
