d725dd7c85b49181e4c0d97f7bcf70d7a23ec1fe
Leanne <76232496+le-anne@users.noreply.github.com>
2021-10-07 12:48:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,47 @@
-const CACHE = "favouriteImageCache";
+// This is the "Offline page" service worker
 
-self.addEventListener('install', async (event) => {
-  const cache = await caches.open(CACHE);
-  await cache.add("/");
-  await cache.add("/script.js");
-  await cache.add("/style.css");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith((async () => {
-    try {
-      return await fetch(event.request);
-    } catch (error) {
-      const cache = await caches.open(CACHE);
-      return await cache.match(event.request);
-    }
-  })());
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
